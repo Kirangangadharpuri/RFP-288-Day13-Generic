2d0a1f95e208b1d045b7cb9737d1daa257356faa
Kiran <kiranpuri532@gmail.com>
2023-07-09 19:23:25 +0530

    [Kiran]Add\ Find maximum integer value

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the third number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int val = FindeMaxValue.MaximumIntegerNumber(num1, num2, num3);
+            Console.WriteLine("{0} IS GREATER  NUMBER.", val);
+        }
+    }
+}
